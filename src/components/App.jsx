447b561634824afcdd385a63b30c7e28db655ec5
Sara Motyka <saramotyka97@gmail.com>
2023-07-28 12:32:42 +0200

    localstorage fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,21 @@ import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    return (
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    );
+  });
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const addContact = event => {
     const { name, number } = event;
@@ -44,17 +52,6 @@ export const App = () => {
   const deleteContact = id => {
     setContacts(contacts.filter(contact => contact.id !== id));
   };
-
-  useEffect(() => {
-    if ('contacts' in localStorage) {
-      const storedContacts = localStorage.getItem('contacts');
-      setContacts(JSON.parse(storedContacts));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   return (
     <section>
